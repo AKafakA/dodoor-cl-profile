da5622d191f63bbe83836b83428c33b5ff4cba7c
Wei Da <wd312@cl.cam.ac.uk>
2024-08-04 16:20:33 +0100

    create profiles for dodoor

--- profile.py
+++ profile.py
@@ -14,14 +14,34 @@ import geni.rspec.pg as pg
 # Create a portal context.
 pc = portal.Context()
 
+num_nodes = 4
+node_deployment_command = ("nohup java -cp dodoor/target/dodoor-1.0-SNAPSHOT.jar edu.cam.dodoor.ServiceDaemon "
+                           "-c ~/dodoor/config.conf -d false -s false -n true  1>service.out  2>/dev/null &")
+
+scheduler_deployment_command = ("nohup java -cp dodoor/target/dodoor-1.0-SNAPSHOT.jar edu.cam.dodoor.ServiceDaemon "
+                                "-c ~/dodoor/config.conf -d true -s true -n false  1>service.out 2>/dev/null &")
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
- 
+
 # Add a raw PC to the request.
-node = request.RawPC("node")
+hardware_type = "m400"
 
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+nodes = []
+for i in range(num_nodes):
+    node = request.RawPC("node" + str(i))
+    node.hardware_type = hardware_type
+    nodes[i].addService(pg.Execute(shell="sh", command="/local/repository/setup.sh"))
 
+    iface = node.addInterface("if" + str(i))
+    iface.component_id = "eth" + str(i)
+    iface.addAddress(pg.IPv4Address("192.168.1." + str(i), "255.255.255.0"))
+    nodes.append(node)
+
+node0 = nodes[0]
+for i in range(1, num_nodes):
+    link = request.Link(members=[node0, nodes[i]])
+    nodes[i].addService(pg.Execute(shell="sh",
+                                   command=node_deployment_command))
+node0.addService(pg.Execute(shell="sh", command=scheduler_deployment_command))
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# Update package list and install required packages
+echo "Installing required packages..."
+echo y | sudo apt install python3-pip thrift-compiler stress openjdk-17-jdk openjdk-17-jre
+
+# Install Python package
+echo "Installing python package..."
+pip install optparse-pretty
+
+# Download and install Apache Maven
+echo "Downloading and installing Apache Maven..."
+wget https://downloads.apache.org/maven/maven-3/3.9.8/binaries/apache-maven-3.9.8-bin.tar.gz
+tar -zxvf apache-maven-3.9.8-bin.tar.gz
+mv apache-maven-3.9.8 maven
+sudo mv maven/ /opt/maven
+
+# Set environment variables for Maven
+echo "Setting up Maven environment variables..."
+echo 'export M2_HOME=/opt/maven' >> ~/.bashrc
+echo 'export PATH=${M2_HOME}/bin:${PATH}' >> ~/.bashrc
+source ~/.bashrc
+
+# Clone the Git repository
+echo "Cloning the dodoor repository..."
+git clone https://github.com/AKafakA/dodoor.git
+
+# Checkout the specific branch and rebuild
+cd dodoor
+echo "Checking out the exp branch and rebuilding..."
+git checkout exp
+sh rebuild.sh
+
+# Run the configuration generator script
+echo "Running the configuration generator script..."
+python3 ~/deploy/python/scripts/config_generator.py -d ~/deploy/resources/scheduler_ip -n ~/deploy/resources/host_ip -s ~/deploy/resources/scheduler_ip --replay_with_delay True --scheduler-ports 20503,20504
+
+echo "Setup completed successfully!"
