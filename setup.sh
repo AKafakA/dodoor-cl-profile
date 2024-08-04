8e11cd060adf24079b3b5bf58aac65fbbed1af4b
Wei Da <wd312@cl.cam.ac.uk>
2024-08-04 19:45:47 +0100

    fix the setup config

--- setup.sh
+++ setup.sh
@@ -4,12 +4,13 @@
 
 # Update package list and install required packages
 
-sudo chmod -R +rwx /users/asdwb
+sudo su
+chmod -R +rwx /users/asdwb
 cd /users/asdwb
 
 echo "Installing required packages..."
-sudo apt update
-echo y | sudo apt install python3-pip thrift-compiler stress openjdk-17-jdk openjdk-17-jre
+apt update
+echo y | sudo apt install python3-pip thrift-compiler stress openjdk-17-jdk openjdk-17-jre vim
 
 # Install Python package
 echo "Installing python package..."
@@ -17,22 +18,21 @@ pip install optparse-pretty
 
 # Download and install Apache Maven
 echo "Downloading and installing Apache Maven..."
-sudo wget https://downloads.apache.org/maven/maven-3/3.9.8/binaries/apache-maven-3.9.8-bin.tar.gz
-sudo tar -zxvf apache-maven-3.9.8-bin.tar.gz
-sudo mv apache-maven-3.9.8 maven
-sudo mv maven/ /opt/maven
+wget https://downloads.apache.org/maven/maven-3/3.9.8/binaries/apache-maven-3.9.8-bin.tar.gz
+tar -zxvf apache-maven-3.9.8-bin.tar.gz
+mv apache-maven-3.9.8 maven
+mv maven/ /opt/maven
 
 # Set environment variables for Maven
 echo "Setting up Maven environment variables..."
-sudo echo 'export M2_HOME=/opt/maven' >> ~/.bashrc
-sudo echo 'export PATH=${M2_HOME}/bin:${PATH}' >> ~/.bashrc
-sudo source ~/.bashrc
+echo 'export M2_HOME=/opt/maven' >> .bashrc
+echo 'export PATH=${M2_HOME}/bin:${PATH}' >> .bashrc
+source .bashrc
 
 # Clone the Git repository
 echo "Cloning the dodoor repository..."
-sudo git clone https://github.com/AKafakA/dodoor.git
-sudo git config --system --add safe.directory dodoor
-sudo chmod -R +rwx /users/asdwb/dodoor
+git clone https://github.com/AKafakA/dodoor.git
+git config --global --add safe.directory /users/asdwb/dodoor
 
 # Checkout the specific branch and rebuild
 cd dodoor
