a7e3f976af5b8c54f3adabe66e8b81fc8d34ee66
Wei Da <wd312@cl.cam.ac.uk>
2024-08-05 17:25:39 +0100

    avoid to create service out files in the setup shell

--- profile.py
+++ profile.py
@@ -16,14 +16,14 @@ pc = portal.Context()
 user_name = "asdwb"
 num_scheduler_datastore = 1
 num_nodes = 30
-node_deployment_command = ("cd /users/{} && sudo nohup java -cp dodoor/target/dodoor-1.0-SNAPSHOT.jar "
+node_deployment_command = ("cd /users/{} && nohup java -cp dodoor/target/dodoor-1.0-SNAPSHOT.jar "
                            "edu.cam.dodoor.ServiceDaemon"
-                           "-c ~/dodoor/config.conf -d false -s false -n true  1>service.out  2>/dev/null &"
+                           "-c ~/dodoor/config.conf -d false -s false -n true  &"
                            .format(user_name))
 
 scheduler_deployment_command = ("cd /users/{} && sudo nohup java -cp dodoor/target/dodoor-1.0-SNAPSHOT.jar "
                                 "edu.cam.dodoor.ServiceDaemon"
-                                "-c ~/dodoor/config.conf -d true -s true -n false  1>service.out 2>/dev/null &"
+                                "-c ~/dodoor/config.conf -d true -s true -n false &"
                                 .format(user_name))
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
