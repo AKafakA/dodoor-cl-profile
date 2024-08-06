7950ec58c24025a5c4a993d43e8afae19336f1bb
Wei Da <wd312@cl.cam.ac.uk>
2024-08-06 18:23:15 +0100

    new topology

--- profile.py
+++ profile.py
@@ -18,28 +18,49 @@ num_scheduler_datastore = 1
 num_nodes = 50
 node_deployment_command = ("cd /users/{} && nohup java -cp dodoor/target/dodoor-1.0-SNAPSHOT.jar "
                            "edu.cam.dodoor.ServiceDaemon"
-                           "-c ~/dodoor/config.conf -d false -s false -n true  &"
+                           " -c ~/dodoor/config.conf -d false -s false -n true  &"
                            .format(user_name))
 
 scheduler_deployment_command = ("cd /users/{} && nohup java -cp dodoor/target/dodoor-1.0-SNAPSHOT.jar "
                                 "edu.cam.dodoor.ServiceDaemon"
-                                "-c ~/dodoor/config.conf -d true -s true -n false &"
+                                " -c ~/dodoor/config.conf -d true -s true -n false &"
                                 .format(user_name))
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
 
 # Add a raw PC to the request.
-hardware_type = "m400"
-link = request.Link()
-nodes = []
-for i in range(num_nodes + num_scheduler_datastore):
-    node = request.RawPC("node" + str(i))
-    node.hardware_type = hardware_type
-    node.addService(pg.Execute(shell="sh", command="sudo ./local/repository/setup.sh {}".format(num_nodes)))
-    link.addNode(node)
-    nodes.append(node)
+executor_hardware_type = "m400"
+scheduler_hardware_type = "d6515"
 
-for i in range(0, num_nodes):
-    nodes[i + num_scheduler_datastore].addService(pg.Execute(shell="sh",
-                                                             command=node_deployment_command))
+link = request.Link()
+num_node_in_link = 50
+links = [link]
+executor_nodes = []
+NETMASK = "255.255.255.0"
+IP_PREFIX = "10.10.1."
+
+for i in range(num_scheduler_datastore, num_nodes + num_scheduler_datastore):
+    node = request.RawPC("node" + str(i))
+    node.hardware_type = executor_hardware_type
+    node.addService(pg.Execute(shell="sh", command="sudo ./local/repository/setup.sh {}".format(num_nodes)))
+    iface = node.addInterface("if" + str(i))
+    iface.addAddress(pg.IPv4Address(IP_PREFIX + str(i), NETMASK))
+    link.addInterface(iface)
+    executor_nodes.append(node)
+    if i % num_node_in_link == 0:
+        link = request.Link()
+        links.append(link)
+
+for i in range(0, num_scheduler_datastore):
+    scheduler_node = request.RawPC("node" + str(i))
+    scheduler_node.hardware_type = scheduler_hardware_type
+    scheduler_node.addService(pg.Execute(shell="sh", command="sudo ./local/repository/setup.sh {}".format(num_nodes)))
+    for link in links:
+        iface = scheduler_node.addInterface("if" + str(i))
+        iface.addAddress(pg.IPv4Address(IP_PREFIX + str(i), NETMASK))
+        link.addInterface(iface)
+
+# for i in range(0, num_nodes):
+#     executor_nodes[i].addService(pg.Execute(shell="sh", command=node_deployment_command))
+
 pc.printRequestRSpec(request)
