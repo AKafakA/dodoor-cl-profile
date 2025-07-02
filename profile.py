080b4e81599bba2fbdf4660398553c55b3aebb81
Wei Da <wd312@cl.cam.ac.uk>
2025-07-02 17:53:26 +0100

    remove dummy ip

--- profile.py
+++ profile.py
@@ -50,8 +50,6 @@ link.bandwidth = bandwidth
 num_node_in_link = 50
 links = [link]
 executor_nodes = []
-NETMASK = "255.255.255.0"
-IP_PREFIX = "10.10.1."
 
 for i in range(num_scheduler_datastore, num_nodes + num_scheduler_datastore):
     node = request.RawPC("node" + str(i))
@@ -65,9 +63,7 @@ for i in range(num_scheduler_datastore, num_nodes + num_scheduler_datastore):
     node.hardware_type = hardware_type
     executor_nodes_mapping[hardware_type] -= 1
     node.addService(pg.Execute(shell="sh", command="sudo ./local/repository/setup.sh {}".format(num_nodes)))
-    iface = node.addInterface("if" + str(i))
-    iface.addAddress(pg.IPv4Address(IP_PREFIX + str(i), NETMASK))
-    link.addInterface(iface)
+    link.addNode(node)
     executor_nodes.append(node)
     if i % num_node_in_link == 0 and i != num_nodes + num_scheduler_datastore - 1:
         link = request.Link()
@@ -81,9 +77,7 @@ for i in range(0, num_scheduler_datastore):
     scheduler_node.hardware_type = scheduler_hardware_type
     scheduler_node.addService(pg.Execute(shell="sh", command="sudo ./local/repository/setup.sh {}".format(num_nodes)))
     for j in range(len(links)):
-        iface = scheduler_node.addInterface("sif" + str(i) + str(j))
-        iface.addAddress(pg.IPv4Address(IP_PREFIX + str(i), NETMASK))
-        links[j].addInterface(iface)
+        links[j].addNode(scheduler_node)
 
 # for i in range(0, num_nodes):
 #     executor_nodes[i].addService(pg.Execute(shell="sh", command=node_deployment_command))
