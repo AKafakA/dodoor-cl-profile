42f8c3556286d789f1eabad736e650cbee67805a
Wei Da <wd312@cl.cam.ac.uk>
2025-08-04 15:00:27 +0100

    support cara

--- profile.py
+++ profile.py
@@ -14,67 +14,30 @@ import geni.rspec.pg as pg
 
 # Create a portal context.
 pc = portal.Context()
-num_scheduler_datastore = 1
 
 executor_nodes_mapping = {
-    "m510": 40,
-    "xl170": 25,
-    "c6525-25g": 18,
-    "c6620": 17,
+    "c240g5": 16,
+    "c4130": 2,
+    "d7525": 12,
+    "d8545": 2,
 }
 
 num_nodes = sum(executor_nodes_mapping.values())
 
-
-# user_name = "asdwb"
-# node_deployment_command = ("cd /users/{} && nohup java -cp dodoor/target/dodoor-1.0-SNAPSHOT.jar "
-#                            "edu.cam.dodoor.ServiceDaemon"
-#                            " -c ~/dodoor/config.conf -d false -s false -n true  &"
-#                            .format(user_name))
-#
-# scheduler_deployment_command = ("cd /users/{} && nohup java -cp dodoor/target/dodoor-1.0-SNAPSHOT.jar "
-#                                 "edu.cam.dodoor.ServiceDaemon"
-#                                 " -c ~/dodoor/config.conf -d true -s true -n false &"
-#                                 .format(user_name))
-
-
-# Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
 
 # Add a raw PC to the request.
-scheduler_hardware_type = "d6515"
 link = request.Link()
 link._best_effort = True
-num_node_in_link = 50
-links = [link]
-executor_nodes = []
 
-for i in range(num_scheduler_datastore, num_nodes + num_scheduler_datastore):
+for i in range(0, num_nodes):
     node = request.RawPC("node" + str(i))
-    # hardware_type = random.choice(list(executor_nodes_mapping.keys()))
-    # if hardware_type in executor_nodes_mapping and executor_nodes_mapping[hardware_type] > 0:
-    #     node.hardware_type = hardware_type
-    #     executor_nodes_mapping[hardware_type] -= 1
     hardware_type = random.choice(list(executor_nodes_mapping.keys()))
     while executor_nodes_mapping[hardware_type] <= 0:
         hardware_type = random.choice(list(executor_nodes_mapping.keys()))
     node.hardware_type = hardware_type
     executor_nodes_mapping[hardware_type] -= 1
     link.addNode(node)
-    executor_nodes.append(node)
-    if i % num_node_in_link == 0 and i != num_nodes + num_scheduler_datastore - 1:
-        link = request.Link()
-        # in kbps
-        link._best_effort = True
-        links.append(link)
-
-
-for i in range(0, num_scheduler_datastore):
-    scheduler_node = request.RawPC("node" + str(i))
-    scheduler_node.hardware_type = scheduler_hardware_type
-    scheduler_node.addService(pg.Execute(shell="sh", command="sudo ./local/repository/setup.sh {}".format(num_nodes)))
-    for j in range(len(links)):
-        links[j].addNode(scheduler_node)
 
 # for i in range(0, num_nodes):
 #     executor_nodes[i].addService(pg.Execute(shell="sh", command=node_deployment_command))
