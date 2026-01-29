67a45aac26097571d8fc664bc1ee43473b1ba727
Wei Da <daweiuestc@gmail.com>
2026-01-29 16:14:37 +0000

    set v100 node as 3

--- profile.py
+++ profile.py
@@ -25,7 +25,7 @@ pc = portal.Context()
 # For small experiments, we can use a fixed mapping of hardware types to node counts.
 executor_nodes_mapping = {
     "c240g5": 12,
-    "c4130": 2
+    "c4130": 3
 }
 
 num_nodes = sum(executor_nodes_mapping.values())
