38d12008ed41643f927cf1eb4518b50bd3520587
Wei Da <daweiuestc@gmail.com>
2026-01-29 16:09:23 +0000

    only keep no amper gpu

--- profile.py
+++ profile.py
@@ -25,8 +25,7 @@ pc = portal.Context()
 # For small experiments, we can use a fixed mapping of hardware types to node counts.
 executor_nodes_mapping = {
     "c240g5": 16,
-    "c4130": 2,
-    "d8545": 2,
+    "c4130": 2
 }
 
 num_nodes = sum(executor_nodes_mapping.values())
